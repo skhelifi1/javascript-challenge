e3f39616c04e867cdffb07158f607670ef2438bb
Sirine <sirine.khelifi1@outlook.com>
2020-12-27 16:48:57 -0500

    added table

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,1 +1,10 @@
-let tableData = data;
+// let tableData = data;
+let tbody= d3.select("tbody");
+data.forEach((ufoReport) => {
+    console.log(ufoReport);
+    let row= tbody.append("tr"); 
+    Object.entries(ufoReport).forEach(([Key,value])=> {
+        console.log(key, value);
+        let cell= row.append("td");
+        cell.text(value);});
+});
